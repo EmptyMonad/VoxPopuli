a752d9ac3e40f59c1b3d18d5c7acda78eb3541be
dconwell <313-dconwell@users.noreply.gitlab.tamucc.edu>
2021-10-29 20:34:01 -0500

    Registration Page Functionality. Everythign done except for Insert statement. Also needing to be wrapped up in bootsrap css

--- WebPages/MainPage.aspx.cs
+++ WebPages/MainPage.aspx.cs
@@ -14,35 +14,14 @@ namespace MapProject.WebPages
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            SqlConnection db = new SqlConnection(SqlDataSource1.ConnectionString);
-            SqlCommand district1count = new SqlCommand("SELECT UpVote FROM VotingInformation WHERE DistrictNumber = 1 ", db);
-            
-            db.Open();
-            {
-              
-                int district1value = (Int32)district1count.ExecuteScalar();
-                if (district1value > 3)
-                {
-                    ScriptManager.RegisterStartupScript(this.Page, Page.GetType(), "MainPageMap.js", "paintDistrictBlack(district1)", true);
-                }
-               
-            }
-            db.Close();
+          
           
 
         }
 
         protected void btn_Click(object sender, EventArgs e)
         {
-            SqlConnection db = new SqlConnection(SqlDataSource1.ConnectionString);
-            SqlCommand district1Name = new SqlCommand("SELECT DistrictName FROM VotingInformation WHERE DistrictNumber = 1", db);
-
-            db.Open();
-            {
-                string district1Namestring = (String)district1Name.ExecuteScalar();
-                RepTitle.InnerText = "This is " + district1Namestring;
-            }
-            db.Close();
+           
 
         }
             
